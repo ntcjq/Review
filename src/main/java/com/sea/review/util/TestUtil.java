20744f868287d7a79784bcf7ebd255cb88f25bf7
Sea <754119493@qq.com>
2022-09-15 11:15:39 +0800

    pinyin util

---
+++ src/main/java/com/sea/review/util/TestUtil.java
@@ -0,0 +1,14 @@
+package com.sea.review.util;
+
+import cn.hutool.extra.pinyin.PinyinUtil;
+
+public class TestUtil {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(PinyinUtil.getPinyin("实践是检验整理的唯一标准"));
+
+    }
+}
